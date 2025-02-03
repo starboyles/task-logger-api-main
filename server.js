e05bb5af15c5caa2be172221d69032cfd10c0405
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-02-03 22:13:19 +0000

    chore: remove database connection code

--- server.js
+++ server.js
@@ -4,18 +4,8 @@ const app = require('./app');
 
 dotenv.config();
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
-mongoose
-  .connect(DB, 
-    )
-  .then(() => console.log('DB Connection Successful'))
-  .catch((err) => console.error('DB Connection Error:', err));
-
 const port =  6000;
 
 app.listen(port, () => {
-  console.log(`Tracker running on port ${port}.... `);
+  console.log(`Task Logger running on port ${port}.... `);
 });
