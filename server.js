0e72620af97babe5fa4f84eb91be6aaae6b75142
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-02-03 23:03:38 +0000

    chore: standardize string quotes in server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-const { admin, db } = require("./config/firebase.config");
+const { admin, db } = require('./config/firebase.config');
 
 dotenv.config();
 
-const port =  6000;
+const port = 6000;
 
 app.listen(port, () => {
   console.log(`Task Logger running on port ${port}.... `);
